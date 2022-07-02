3627c9501944b7032e15e2ed47bf9182945ce361
Administrator <193356401@qq.com>
2022-07-02 19:02:35 +0800

    second2

--- nets/Fusion.py
+++ nets/Fusion.py
@@ -29,20 +29,20 @@ class fusion(nn.Module):
         self.avgpool=nn.AvgPool2d(kernel_size=3,stride=2,padding=1)
 
         self.conv1=nn.Conv2d(in_channels=64,out_channels=256,kernel_size=3,stride=2,padding=1)
-        self.b1=nn.BatchNorm2d(256,affine=True)
+
         self.conv2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.b2 = nn.BatchNorm2d(256, affine=True)
+
         self.conv1_1=nn.Conv2d(768,256,1)
         self.l1 = L2Norm(64, 2)
         self.l2 = L2Norm(128, 2)
     def forward(self,left,middle,right):
         x1=self.maxpool(left)
         x1=self.l1(x1)
-        x1=self.b1(self.conv1(x1))
+        x1=self.conv1(x1)
         x1=F.relu(x1)
         x2=self.avgpool(middle)
         x2=self.l2(x2)
-        x2=self.b2(self.conv2(x2))
+        x2=self.conv2(x2)
         x2 = F.relu(x2)
         sum=torch.cat([x1,x2,right],dim=1)
         sum=self.conv1_1(sum)
@@ -53,7 +53,8 @@ if __name__ == '__main__':
     middle = torch.randn(1, 128, 40, 40)
     right=torch.randn(1,256,20,20)
     fusion=fusion()
-    fusion(left,middle,right)
+    s=fusion(left,middle,right)
+    print(s.shape)
 
 
 
